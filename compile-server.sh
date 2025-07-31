7414a2b073052d4beb8d6130cfcd335117f826b5
AskeFC <AskeFC@gmail.com>
2025-07-31 11:09:04 +0200

    Fixes
    Fixed compile script running in bash shell.
    Fixed build script not using cache.

--- compile-server.sh
+++ compile-server.sh
@@ -2,4 +2,4 @@
 
 docker buildx build -t darkeden:build . -f Dockerfile.build --no-cache
 
-docker run --rm -v $(pwd):/home/darkeden/vs darkeden:build make clean && make -j4
+docker run --rm -v $(pwd):/home/darkeden/vs darkeden:build bash -c "make clean && make -j4"

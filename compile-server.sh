61776ddd2a979d87f8ea18531aa338a403beead6
AskeFC <AskeFC@gmail.com>
2026-04-16 14:11:53 +0200

    Update
    Upgrade lua from 5.4 to 5.5
    
    Add -fno-strict-aliasing to compile flags
    
    Use cachyos-v3:latest in containers
    
    Force docker to pull again when building.

--- compile-server.sh
+++ compile-server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker buildx build -t darkeden:build . -f Dockerfile.build --no-cache
+docker buildx build -t darkeden:build . -f Dockerfile.build --no-cache --pull
 
 docker run --rm -v $(pwd):/home/darkeden/vs darkeden:build bash -c "make clean && make -j3"
